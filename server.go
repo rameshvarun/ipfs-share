566122b56186f3c8a1ae1b0794dba295f7d90714
Varun Ramesh <varunramesh@live.com>
2015-02-22 19:09:59 -0800

    File Upload

--- server.go
+++ server.go
@@ -9,6 +9,9 @@ import (
 	"os/exec"
 	"strings"
 	"fmt"
+	"mime/multipart"
+	"io"
+	"net/http"
 
 	"github.com/go-martini/martini"
 	"github.com/codegangsta/martini-contrib/render"
@@ -20,15 +23,19 @@ type Paste struct {
     Content            string  `form:"content" binding:"required"`
 }
 
+type UploadFile struct {
+    File *multipart.FileHeader `form:"file" binding:"required"`
+}
+
 func main() {
 	// Create content storage directories
 	if err := os.MkdirAll(path.Join("files","text"), 0755); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.MkdirAll(path.Join("files","video"), 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files","images"), 0755); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.MkdirAll(path.Join("files","images"), 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files","other"), 0755); err != nil {
 		log.Fatal(err)
 	}
 
@@ -46,6 +53,7 @@ func main() {
 	m := martini.Classic()
 	m.Use(render.Renderer())
 	m.Post("/paste", binding.Form(Paste{}), func(paste Paste, ferr binding.Errors, r render.Render) {
+		// Write content to a file
 		filename := time.Now().Format(time.UnixDate) + ".txt"
 		filepath := path.Join("files","text", filename)
 
@@ -56,16 +64,51 @@ func main() {
 		f.WriteString(paste.Content)
 		f.Close()
 
+		// Add this file to IPFS
 		cmd := exec.Command("ipfs", "add", filepath)
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		// Create a share URL and return
 		words := strings.Split(string(output[:]), " ")
 		hash := words[1]
 		url := *gatewayURL + "/ipfs/" + hash
+		response := map[string]interface{}{ "url": url }
+		r.JSON(200, response)
+	})
 
+	m.Post("/upload", func(req *http.Request, r render.Render) {
+		infile, header, err := req.FormFile("file")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer infile.Close()
+
+		outfilename := header.Filename
+		outfilepath := path.Join("files","other", outfilename)
+
+		outfile, err := os.Create(outfilepath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("Piping upload into disk file.")
+		io.Copy(outfile, infile)
+		outfile.Close()
+
+		// Add to IPFS
+		cmd := exec.Command("ipfs", "add", filepath)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create a File URL and return
+		words := strings.Split(string(output[:]), " ")
+		hash := words[1]
+		url := *gatewayURL + "/ipfs/" + hash
 		response := map[string]interface{}{ "url": url }
 		r.JSON(200, response)
 	})
