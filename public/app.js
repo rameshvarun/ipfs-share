b2cfcbf4243310198b8793e34ae22da1a4815878
Varun Ramesh <varunramesh@live.com>
2015-02-22 17:42:04 -0800

    Pastebin system

---
+++ public/app.js
@@ -0,0 +1,23 @@
+$(function() {
+	$("#pastebin-form").submit(function(e) {
+		e.preventDefault();
+
+		var file_content = $("#pastebin-content").val()
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			data: {
+				content : $("#pastebin-content").val()
+			},
+			url: "/paste",
+			success: function(data) {
+				$("#pastebin-content").val("");
+				window.open(data.url, '_blank')
+			},
+			error : function(jqXHR, status, message) {
+				sweetAlert(status, message, "error");
+			}
+		});
+		
+	})
+})
--- server.go
+++ server.go
@@ -4,25 +4,37 @@ import (
 	"os"
 	"flag"
 	"log"
+	"time"
+	"path"
+	"os/exec"
+	"strings"
+	"fmt"
 
 	"github.com/go-martini/martini"
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/boltdb/bolt"
+	"github.com/martini-contrib/binding"
 )
+
+type Paste struct {
+    Content            string  `form:"content" binding:"required"`
+}
 
 func main() {
 	// Create content storage directories
-	if err := os.MkdirAll("files/text", 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files","text"), 0755); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.MkdirAll("files/video", 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files","video"), 0755); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.MkdirAll("files/images", 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files","images"), 0755); err != nil {
 		log.Fatal(err)
 	}
 
-	//gatewayURL := flag.String("gateway", "http://gateway.ipfs.io", "The HTTP gateway used in shared file links.")
+	gatewayURL := flag.String("gateway", "http://gateway.ipfs.io", "The HTTP gateway used in shared file links.")
+	port := flag.Int("port", 3000, "The port number to run the server on.")
+	hostname := flag.String("hostname", "0.0.0.0", "The hostname to run under.")
 	flag.Parse()
 
 	db, err := bolt.Open("blog.db", 0600, nil)
@@ -33,5 +45,30 @@ func main() {
 
 	m := martini.Classic()
 	m.Use(render.Renderer())
-	m.Run()
+	m.Post("/paste", binding.Form(Paste{}), func(paste Paste, ferr binding.Errors, r render.Render) {
+		filename := time.Now().Format(time.UnixDate) + ".txt"
+		filepath := path.Join("files","text", filename)
+
+		f, err := os.Create(filepath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.WriteString(paste.Content)
+		f.Close()
+
+		cmd := exec.Command("ipfs", "add", filepath)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		words := strings.Split(string(output[:]), " ")
+		hash := words[1]
+		url := *gatewayURL + "/ipfs/" + hash
+
+		response := map[string]interface{}{ "url": url }
+		r.JSON(200, response)
+	})
+
+	m.RunOnAddr(fmt.Sprintf("%s:%d",*hostname, *port))
 }
