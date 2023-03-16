92322b6a7c21a913dc2f1fa0a7a1456856fd1848
Varun Ramesh <varunramesh@live.com>
2023-03-16 12:51:37 -0700

    Formatting + use https for IPFS gateway.

--- server.go
+++ server.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"os"
-	"flag"
-	"log"
-	"time"
-	"path"
-	"os/exec"
-	"strings"
-	"fmt"
-	"mime/multipart"
-	"io"
-	"net/http"
 	"crypto/md5"
 	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"time"
 
-	"github.com/go-martini/martini"
-	"github.com/codegangsta/martini-contrib/render"
 	"github.com/boltdb/bolt"
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/go-martini/martini"
 	"github.com/martini-contrib/binding"
 	"github.com/vincent-petithory/dataurl"
 )
 
 type Paste struct {
-    Content            string  `form:"content" binding:"required"`
+	Content string `form:"content" binding:"required"`
 }
 
 type Image struct {
-    DataURL           string  `form:"dataurl" binding:"required"`
+	DataURL string `form:"dataurl" binding:"required"`
 }
 
 type UploadFile struct {
-    File *multipart.FileHeader `form:"file" binding:"required"`
+	File *multipart.FileHeader `form:"file" binding:"required"`
 }
 
 func main() {
 	// Create content storage directories
-	if err := os.MkdirAll(path.Join("files","text"), 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files", "text"), 0755); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.MkdirAll(path.Join("files","images"), 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files", "images"), 0755); err != nil {
 		log.Fatal(err)
 	}
-	if err := os.MkdirAll(path.Join("files","other"), 0755); err != nil {
+	if err := os.MkdirAll(path.Join("files", "other"), 0755); err != nil {
 		log.Fatal(err)
 	}
 
-	gatewayURL := flag.String("gateway", "http://gateway.ipfs.io", "The HTTP gateway used in shared file links.")
+	gatewayURL := flag.String("gateway", "https://gateway.ipfs.io", "The HTTP gateway used in shared file links.")
 	port := flag.Int("port", 3000, "The port number to run the server on.")
 	hostname := flag.String("hostname", "0.0.0.0", "The hostname to run under.")
 	flag.Parse()
@@ -71,7 +71,7 @@ func main() {
 	m.Post("/paste", binding.Form(Paste{}), func(paste Paste, ferr binding.Errors, r render.Render) {
 		// Write content to a file
 		filename := time.Now().Format(time.UnixDate) + ".txt"
-		filepath := path.Join("files","text", filename)
+		filepath := path.Join("files", "text", filename)
 
 		f, err := os.Create(filepath)
 		if err != nil {
@@ -91,7 +91,7 @@ func main() {
 		words := strings.Split(string(output[:]), " ")
 		hash := words[1]
 		url := *gatewayURL + "/ipfs/" + hash
-		response := map[string]interface{}{ "url": url }
+		response := map[string]interface{}{"url": url}
 		r.JSON(200, response)
 	})
 
@@ -104,7 +104,7 @@ func main() {
 
 		// Write content to a file
 		filename := time.Now().Format(time.UnixDate)
-		filepath := path.Join("files","images", filename)
+		filepath := path.Join("files", "images", filename)
 
 		f, err := os.Create(filepath)
 		if err != nil {
@@ -124,7 +124,7 @@ func main() {
 		words := strings.Split(string(output[:]), " ")
 		hash := words[1]
 		url := *gatewayURL + "/ipfs/" + hash
-		response := map[string]interface{}{ "url": url }
+		response := map[string]interface{}{"url": url}
 		r.JSON(200, response)
 	})
 
@@ -140,12 +140,12 @@ func main() {
 		hasher := md5.New()
 		io.WriteString(hasher, header.Filename)
 		dirname := hex.EncodeToString(hasher.Sum(nil))[:5]
-		if err := os.MkdirAll(path.Join("files","other", dirname), 0755); err != nil {
+		if err := os.MkdirAll(path.Join("files", "other", dirname), 0755); err != nil {
 			log.Fatal(err)
 		}
 
 		outfilename := header.Filename
-		outfilepath := path.Join("files","other", dirname, outfilename)
+		outfilepath := path.Join("files", "other", dirname, outfilename)
 
 		outfile, err := os.Create(outfilepath)
 		if err != nil {
@@ -157,7 +157,7 @@ func main() {
 		outfile.Close()
 
 		// Add to IPFS
-		cmd := exec.Command("ipfs", "add", "-r", path.Join("files","other", dirname))
+		cmd := exec.Command("ipfs", "add", "-r", path.Join("files", "other", dirname))
 		output, err := cmd.Output()
 		log.Println(string(output))
 		if err != nil {
@@ -166,12 +166,12 @@ func main() {
 
 		// Create a File URL and return
 		lines := strings.Split(string(output[:]), "\n")
-		words := strings.Split(lines[len(lines) - 2], " ")
+		words := strings.Split(lines[len(lines)-2], " ")
 		hash := words[1]
 		url := *gatewayURL + "/ipfs/" + hash + "/" + outfilename
-		response := map[string]interface{}{ "url": url }
+		response := map[string]interface{}{"url": url}
 		r.JSON(200, response)
 	})
 
-	m.RunOnAddr(fmt.Sprintf("%s:%d",*hostname, *port))
+	m.RunOnAddr(fmt.Sprintf("%s:%d", *hostname, *port))
 }
